8c992bd78211cf715f879c9a6b36a5871a6d38b3
pooja-pt5401 <poojapriyadarshnips@gmail.com>
2022-07-11 17:38:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.stopwatch
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // It will show a message on the screen
+        // then onStart is invoked
+        val toast  = Toast.makeText(applicationContext, "onStart Called", Toast.LENGTH_LONG).show()
+    }
+
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+}
